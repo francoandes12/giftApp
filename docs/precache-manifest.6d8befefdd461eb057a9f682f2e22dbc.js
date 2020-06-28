a59729074943e65d2dad71ef91963ed37d4b2774
francoandes12 <francoande12@gmail.com>
2020-06-27 23:38:38 -0300

    version 2

---
+++ docs/precache-manifest.6d8befefdd461eb057a9f682f2e22dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882b5c6d424abb2bb1ef9a1a3ab2ff38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "823fa5820cadc05b6b73",
+    "url": "/static/css/main.fd772683.chunk.css"
+  },
+  {
+    "revision": "5bac94bff2391ec275f8",
+    "url": "/static/js/2.68b72264.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68b72264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823fa5820cadc05b6b73",
+    "url": "/static/js/main.8455b138.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
